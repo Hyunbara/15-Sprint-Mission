3af9d055151b7146144b6ae4369c683e6f63d819
kimyh <ikimyh059554@gmail.com>
2025-03-16 23:37:41 +0900

    feat: 로그인/회원 가입 유효성 검사 기능 추가 및 공통 js 추가

--- auth/login-validation.js
+++ auth/login-validation.js
@@ -1,37 +1,23 @@
-// input 태그의 focus 이벤트
-
-// 이메일, 패스워드 input 요소들.
-const emailInput = document.querySelector("#form-email");
-const passwordInput = document.querySelector("#form-password");
-
-// 에러 메세지를 담은 p태그 요소들.
-const emailError = document.querySelector("#email-error");
-const passwordError = document.querySelector("#password-error");
-
-// 이메일 유효성 검사 패턴
-const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-// 로그인 버튼 (login.html)
-const loginBtn = document.querySelector("#btn-login");
-
-let isEmailValid = false;
-let isPasswordValid = false;
+import { emailInput, passwordInput, emailPattern, emailError, passwordError, loginBtn, isEmailValid, isPasswordValid } from "./validation-common.js";
 
 // 이메일 유효성 검사 함수
 const emailCheck = () => {
   // 이메일 값 공백 제거 후 가져오기.
   const emailValue = emailInput.value.trim();
-  isEmailValid = false;
+  isEmailValid.value = false;
 
   if (emailValue === "") {
     emailError.textContent = "이메일을 입력해주세요.";
     emailError.style.display = "block";
+    emailInput.classList.add("input-error");
   } else if (!emailPattern.test(emailValue)) {
     emailError.textContent = "잘못된 이메일 형식입니다.";
     emailError.style.display = "block";
+    emailInput.classList.add("input-error");
   } else {
     emailError.style.display = "none";
-    isEmailValid = true;
+    emailInput.classList.remove("input-error");
+    isEmailValid.value = true;
   }
 
   loginBtnToggle();
@@ -40,17 +26,20 @@ const emailCheck = () => {
 // 패스워드 유효성 검사 함수
 const passwordCheck = () => {
   const passwordValue = passwordInput.value.trim();
-  isPasswordValid = false;
+  isPasswordValid.value = false;
 
   if (passwordValue === "") {
     passwordError.textContent = "비밀번호를 입력해주세요.";
     passwordError.style.display = "block";
+    passwordInput.classList.add("input-error");
   } else if (passwordValue.length < 8) {
     passwordError.textContent = "비밀번호를 8자 이상 입력해주세요.";
     passwordError.style.display = "block";
+    passwordInput.classList.add("input-error");
   } else {
     passwordError.style.display = "none";
-    isPasswordValid = true;
+    passwordInput.classList.remove("input-error");
+    isPasswordValid.value = true;
   }
 
   loginBtnToggle();
@@ -58,21 +47,18 @@ const passwordCheck = () => {
 
 // 이메일, 패스워드 유효성 검사에 따른 로그인 버튼 활성화 함수
 const loginBtnToggle = () => {
-  if (isEmailValid && isPasswordValid) {
+  if (isEmailValid.value && isPasswordValid.value) {
     loginBtn.style.backgroundColor = "#3578e5";
     loginBtn.style.cursor = "pointer";
     loginBtn.disabled = false;
   } else {
+    loginBtn.style.backgroundColor = "#9ca3af";
+    loginBtn.style.color = "#f3f4f6";
+    loginBtn.style.cursor = "not-allowed";
     loginBtn.disabled = true;
   }
 };
 
-// 이메일 요소 focusout 이벤트
+// focusout 이벤트
 emailInput.addEventListener("focusout", emailCheck);
-
-// 패스워드 요소 focusout 이벤트
 passwordInput.addEventListener("focusout", passwordCheck);
-
-// 로그인 버튼 요소 초기 상태 설정
-loginBtn.style.cursor = "not-allowed";
-loginBtn.disabled = true;
