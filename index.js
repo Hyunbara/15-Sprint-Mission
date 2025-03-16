69896ca495a03877fc8a8daaa27ee2f28ceff847
kimyh <ikimyh059554@gmail.com>
2025-03-16 20:42:41 +0900

    chore: pages 폴더 내 불필요한 파일 삭제(css)

--- index.js
+++
@@ -1,8 +0,0 @@
-import { mobileChangeImg } from "./ui/mobile_changeImg.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  // 모바일 화면에서 로고 변경 함수 실행
-  mobileChangeImg();
-
-  window.addEventListener("resize", mobileChangeImg);
-});
