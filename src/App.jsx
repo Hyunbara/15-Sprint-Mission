5218ff044be529eff5b27acdd2bac922f2f374ad
kimyh <ikimyh059554@gmail.com>
2025-04-02 21:51:38 +0900

    feat: 라우터 및 라우팅 네비게이션 구조 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Items from "./components/Items";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>환경 설정</div>
-    </>
+    <BrowserRouter>
+      <header>
+        판다마켓
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/board">자유게시판</NavLink>
+          <NavLink to="/items">중고마켓</NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path="/board" element={<h1>board</h1>} />
+        <Route path="/items" element={<Items />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
