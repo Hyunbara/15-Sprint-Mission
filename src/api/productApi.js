fb41d41b17a4078b628f2cb9f77f73fa8ad9b3e5
Hyun <ikimyh059554@gmail.com>
2025-04-08 13:32:34 +0900

    refactor: 파라미터로 query를 받도록 수정

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,5 +1,5 @@
-export const getProducts = async () => {
-  const response = await fetch("https://panda-market-api.vercel.app/products");
+export const getProducts = async (query = "") => {
+  const response = await fetch(`https://panda-market-api.vercel.app/products${query}`);
   const data = await response.json();
   return data;
 };
