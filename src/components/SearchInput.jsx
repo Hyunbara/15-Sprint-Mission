2cabb8f5640f836cd0c2510e075f7b0566880347
Hyun <ikimyh059554@gmail.com>
2025-04-10 14:18:02 +0900

    feat: 재사용성을 고려하여 상품 검색 입력 폼 컴포넌트 추가

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./SearchInput.css";
+
+/**
+ * 사용자가 입력한 검색어를 상위 컴포넌트로 전달하는 컴포넌트
+ *  onChange 이벤트 방식으로 실시간으로 동작
+ * @param {onSearch} props - 검색어 전달 함수
+ *
+ */
+const SearchInput = ({ onSearch }) => {
+  const [keyword, setKeyword] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setKeyword(value);
+
+    onSearch(value.trim()); // 공백 제거 후 상위 컴포넌트로 전달
+  };
+
+  return <input type="text" value={keyword} placeholder="🔎  검색할 상품을 입력해주세요." onChange={handleChange} className="search-input" />;
+};
+
+export default SearchInput;
