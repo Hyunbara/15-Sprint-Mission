ea49b343a041b81a019de315091a58e79972bc20
Hyun <ikimyh059554@gmail.com>
2025-04-10 20:58:48 +0900

    chore: 컴포넌트 주석 작성

--- src/components/BestProducts.jsx
+++ src/components/BestProducts.jsx
@@ -2,6 +2,12 @@ import { getProducts } from "../api/productApi";
 import { useState, useEffect } from "react";
 import "./BestProducts.css";
 
+/**
+ *
+ * 베스트 상품을 보여주는 컴포넌트
+ * 인기순(favorite)으로 정렬된 상품을 보여줍니다. (반응형 크기에 따라 PC: 4 / tablet: 2 / mobile: 1)
+ * resize 함수를 사용하여 화면 크기에 따라 visibleCount를 조절하여  상품을 보여줍니다.
+ */
 const BestProduct = () => {
   const [bestProducts, setBestProducts] = useState([]);
   const [visibleCount, setVisibleCount] = useState(4); // default pc
