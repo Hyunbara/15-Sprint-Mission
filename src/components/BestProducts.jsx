04d76cbdb5e89a4bb2d3d56006289ff975a6eedc
Hyun <ikimyh059554@gmail.com>
2025-04-10 19:22:14 +0900

    feat: resize 시, 마운트 시 width에 따른 카운트 반환 로직 추가

--- src/components/BestProducts.jsx
+++ src/components/BestProducts.jsx
@@ -4,6 +4,7 @@ import "./BestProducts.css";
 
 const BestProduct = () => {
   const [bestProducts, setBestProducts] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(4); // default pc
 
   useEffect(() => {
     getProducts("?page=1&pageSize=4&orderBy=favorite").then((data) => {
@@ -11,19 +12,40 @@ const BestProduct = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const updateVisibleCount = () => {
+      const width = window.innerWidth;
+
+      if (width <= 767) {
+        // mobile
+        setVisibleCount(1);
+      } else if (width <= 1199) {
+        // tablet
+        setVisibleCount(2);
+      } else {
+        setVisibleCount(4);
+      }
+    };
+
+    updateVisibleCount();
+    window.addEventListener("resize", updateVisibleCount); // resize 시 eventListener
+
+    return () => window.removeEventListener("resize", updateVisibleCount);
+  }, []);
+
   return (
     <section className="best-products">
       <h3 className="best-products__title">베스트 상품</h3>
       <div className="best-products__grid">
-        {bestProducts.map((item) => (
-          <div key={item.id} className="product-card">
-            <div className="product-card__image-wrapper">
-              <img src={item.images.length > 0 ? item.images[0] : ""} alt={item.name} className="product-card__image" />
+        {bestProducts.slice(0, visibleCount).map((item) => (
+          <div key={item.id} className="best-products__card">
+            <div className="best-products__image-wrapper">
+              <img src={item.images.length > 0 ? item.images[0] : ""} alt={item.name} className="best-products__image" />
             </div>
-            <div className="product-card__text-group">
-              <p className="product-card__title">{item.name}</p>
-              <p className="product-card__price">{item.price.toLocaleString()}원</p>
-              <p className="product-card__favorite">❤️ {item.favoriteCount}</p>
+            <div className="best-products__text-group">
+              <p className="best-products__title-text">{item.name}</p>
+              <p className="best-products__price">{item.price.toLocaleString()}원</p>
+              <p className="best-products__favorite">❤️ {item.favoriteCount}</p>
             </div>
           </div>
         ))}
