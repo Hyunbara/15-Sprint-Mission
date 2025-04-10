9b9662b583e196a2eb23ad458dd98b06863bd4c2
Hyun <ikimyh059554@gmail.com>
2025-04-10 21:06:15 +0900

    refactor: className 수정

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -4,6 +4,11 @@ import BestProduct from "../components/BestProducts";
 import ProductList from "../components/ProductList";
 import "./Items.css";
 
+/**
+ *
+ * 상품 목록 페이지 컴포넌트
+ * 베스트 상품 컴포넌트(BestProduct)와 전체 상품 목록 컴포넌트 (ProductList)로 구성되어있습니다.
+ */
 const Items = () => {
   const [products, setProducts] = useState([]);
 
@@ -14,7 +19,7 @@ const Items = () => {
   }, []);
 
   return (
-    <div className="items-wrapper">
+    <div className="items">
       <BestProduct />
       <ProductList products={products} />
     </div>
