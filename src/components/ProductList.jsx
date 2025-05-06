f131308489211fb8e0f484ae540ecbec6370cb3b
Hyun <ikimyh059554@gmail.com>
2025-05-06 21:47:16 +0900

    refactor: 피드백 요소 수정 - 상위 컴포넌트에서 api호출 말고 해당 컴포넌트에서 api요청 하여 화면에 뿌려줄 수 있도록 수정

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,29 +4,37 @@ import Pagination from "./Pagination";
 import "./ProductList.css";
 import SearchInput from "./SearchInput";
 import { useNavigate } from "react-router-dom";
+import { getProducts } from "../api/productApi";
+import Dropdown from "./Dropdown";
 
 /**
  *  전체 상품 목록을 카드 형태로 표시하는 컴포넌트 입니다.
  *  검색어 기능, 정렬, 페이징 기능이 포함되어있습니다.
  */
-const ProductList = ({ products }) => {
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
   const [orderBy, setOrderBy] = useState("recent");
   const [page, setPage] = useState(1);
   const [searchKeyword, setSearchKeyword] = useState("");
 
   const nav = useNavigate(); // 페이지 이동을 위한 hook 사용
 
-  const pageSize = 10; // 한 페이지에 보여줄 상품 수
-
   const [responsivePageSize, setResponsivePageSize] = useState(10); // 기본값 10
 
-  // option 값에 따라 정렬된 상품 리스트를 보여줌 (desc)
-  const sortedProducts = [...products].sort((a, b) => {
-    if (orderBy === "favorite") return b.favoriteCount - a.favoriteCount;
-    return new Date(b.createdAt) - new Date(a.createdAt);
-  });
+  useEffect(() => {
+    const params = {
+      page: 1,
+      pageSize: 300,
+      orderBy,
+    };
+    if (searchKeyword) {
+      params.keyword = searchKeyword;
+    }
 
-  const filteredProducts = sortedProducts.filter((product) => product.name.includes(searchKeyword));
+    getProducts(params).then((data) => {
+      setProducts(data.list);
+    });
+  }, [orderBy, searchKeyword]);
 
   useEffect(() => {
     const updatePageSize = () => {
@@ -51,8 +59,8 @@ const ProductList = ({ products }) => {
   }, []);
 
   // 페이징 처리
-  const paginatedProducts = filteredProducts.slice((page - 1) * responsivePageSize, page * responsivePageSize);
-  const totalPage = Math.ceil(filteredProducts.length / responsivePageSize);
+  const paginatedProducts = products.slice((page - 1) * responsivePageSize, page * responsivePageSize);
+  const totalPage = Math.ceil(products.length / responsivePageSize);
 
   const handleOrderChange = (e) => {
     setOrderBy(e.target.value);
@@ -68,16 +76,27 @@ const ProductList = ({ products }) => {
   return (
     <section>
       <div className="product-list__header">
-        <h3>전체 상품</h3>
-        <div className="product-list__actions">
-          <SearchInput onSearch={handleSearch} />
+        <div className="product-list__top">
+          <h3>전체 상품</h3>
           <button onClick={() => nav("/additem")} className="product-list__add-button">
             <span>상품 등록하기</span>
           </button>
-          <select value={orderBy} onChange={handleOrderChange} className="product-list__select">
-            <option value="createdAt">최신순</option>
-            <option value="favorite">좋아요순</option>
-          </select>
+        </div>
+
+        <div className="product-list__actions">
+          <SearchInput onSearch={handleSearch} className="product-list__search-input" />
+          <Dropdown
+            value={orderBy}
+            onSelect={(value) => {
+              setOrderBy(value);
+              setPage(1);
+            }}
+            options={[
+              { value: "recent", label: "최신순" },
+              { value: "favorite", label: "좋아요순" },
+            ]}
+            className="product-list__dropdown"
+          />
         </div>
       </div>
 
